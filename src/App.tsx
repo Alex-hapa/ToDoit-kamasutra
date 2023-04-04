449db4e58eecd08bb355eba153a2086724d9030c
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-04-04 23:19:34 +0300

    add method map for render

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,14 @@ function App() {
     const tasks1 = [
         {id: 1, title: 'HTML&CSS', isDone: true},
         {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'ReactJS', isDone: false}
+        {id: 3, title: 'ReactJS', isDone: false},
+        {id: 4, title: 'ReactJS', isDone: false}
     ]
 
     const tasks2 = [
-        {id: 1, title: 'Hello World', isDone: true},
-        {id: 2, title: 'I am Happy', isDone: false},
-        {id: 3, title: 'Yo', isDone: false}
+        {id: 5, title: 'Hello World', isDone: true},
+        {id: 6, title: 'I am Happy', isDone: false},
+        {id: 7, title: 'Yo', isDone: false}
     ]
 
     return (
