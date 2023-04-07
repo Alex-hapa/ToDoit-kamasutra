fe3a66f42f63ea5436af623c564c8e1e2718da16
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-04-07 22:40:27 +0300

    add task's delete-button

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -12,19 +12,27 @@ type PropsType = {
 }
 
 export function Todolist(props: PropsType) {
-    return <div>
-        <h3>{props.title}</h3>
+    return (
         <div>
-            <input/>
-            <button>+</button>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map((el) => {
+                    return (<li key={el.id}>
+                        <input type="checkbox" checked={el.isDone}/>
+                        <span>{el.title}</span>
+                        <button>X</button>
+                    </li>);
+                })}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
         </div>
-        <ul>
-            {props.tasks.map((el) => <li key={el.id}><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>)}
-        </ul>
-        <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-        </div>
-    </div>
+    );
 }
