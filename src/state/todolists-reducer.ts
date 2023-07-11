e19f8b5216f6e0a59ff8d95a38eac37a63892dcf
Aliaksandr Hapankou <akkforrs@gmail.com>
2023-07-11 12:50:26 +0300

    add code to todolists-reducer to add todolist and write test

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,22 +1,38 @@
 import {TodolistType} from '../App';
+import {v1} from 'uuid';
 
 export const todolistsReducer = (state: Array<TodolistType>, action: TodolistReducerType) => {
     switch (action.type) {
         case 'REMOVE-TODOLIST': {
             return state.filter(el => el.id !== action.payload.id)
         }
+        case 'ADD-TODOLIST':{
+            let newTodolistId = v1();
+            let newTodolist: TodolistType = {id: newTodolistId, title: action.payload.title, filter: 'all'};
+            return [...state, newTodolist]
+        }
         default:
             return state
     }
 }
 
-type TodolistReducerType = RemoveTodolistACType
+type TodolistReducerType = RemoveTodolistACType | AddTodolistACType
 type RemoveTodolistACType = ReturnType<typeof removeTodolistAC>
 export const removeTodolistAC = (id: string) => {
     return {
         type: 'REMOVE-TODOLIST',
         payload: {
             id
+        }
+    } as const
+}
+
+type AddTodolistACType =ReturnType<typeof addTodolistAC>
+export const addTodolistAC = (title: string) => {
+    return {
+        type: 'ADD-TODOLIST',
+        payload: {
+            title
         }
     } as const
 }
